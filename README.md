847206990802c46785f576a0b8349693874f2f8f
LiChi Chang <lichiricky@gmail.com>
2021-11-22 22:18:17 -0500

    first version

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ For my wife
     ```cmd
     git submodule add git@github.com:Li-Chi-Chang/web_browser.git
     ```
+
+* pull with submodule
+
+    ```cmd
+    git clone --recursive https://github.com/Li-Chi-Chang/novel_downloader.git
+    ```
--- __init__.py
+++ __init__.py
@@ -1,53 +1,93 @@
 from gmail_sending import send
-from web_browser import edge_driver
+from web_browser import safari_driver, edge_driver, get_novel_template
 
 from os.path import realpath,dirname,join
 from binascii import unhexlify
 import re, time
+from bs4 import BeautifulSoup
+from urllib import parse
 
-def wwwstocx_clearify_callback(text):
-    codes = re.findall('%[a-z|A-Z|0-9]{2}%[a-z|A-Z|0-9]{2}%[a-z|A-Z|0-9]{2}', text)
-    if(len(codes)>0):
-        for code in codes:
-            chrsplit = re.split('%',code)
-            word = unhexlify(''.join(chrsplit[1:])).decode("utf-8")
-            text = text.replace(code,word)
-    text = re.sub('.本.作.品.由.*','',text)
-    text = re.sub('..本..作..品..由.*','',text)
-    text = re.sub('...本...作...品...由.*','',text)
-    text = re.sub('.思.兔.*','',text)
-    text = re.sub('..思..兔.*','',text)
-    text = re.sub('...思...兔.*','',text)
-    text = re.sub('....思....兔.*','',text)
-    text = re.sub('＊','',text)
-    time.sleep(1)
-    return text
+webenv = 'safari'
+
+def filename_clearify(string, with_space=True):
+    signs = ['?','“','”','/','\\','<','>','*','|',':','&', '+','\'','.','!','"','#',]
+    for sign in signs:
+        string = string.replace(sign,'')
+    if not with_space:
+        string = string.replace(' ','')
+    return string
+
+def clearify(soup, callback = lambda x: x):
+    funcsoup = soup
+    for br in funcsoup.find_all('br'):
+        br.replace_with('\n')
+    originalText = funcsoup.text.strip()
+    originalText = callback(originalText)
+    originalText = re.sub('。','。\n',originalText)
+    originalText = re.sub('\n{1,}','\n',originalText)
+    originalText = re.sub('\t{1,}',' ',originalText)
+    originalText = re.sub('[ ]{1,}',' ',originalText)
+    originalText = re.sub('[　]{1,}',' ',originalText)
+    originalText = re.sub(r'\x00', '', originalText)
+    originalText = re.sub('[ ]{1,}',' ',originalText)
+    originalText = re.sub('[ |\n]{2,}','\n',originalText)
+    return originalText
+
+def get_driver(webenv):
+    if (webenv == 'safari'):
+        return safari_driver()
+    elif(webenv == 'edge'):
+        return edge_driver()
+
+def getnext(soup, currenturl):
+    hasNext = True
+    try:
+        pattern = re.compile('下[一|壹]{0,1}[頁|章|页|篇]')
+        partialurl = soup.find('a', text=pattern)['href']
+        currenturl = parse.urljoin(currenturl,partialurl)
+    except:
+        hasNext = False
+        print('not found a next')
+    return {"url":currenturl, "hasNext":hasNext}
 
 def downloadABook(oneurl):
-    web = edge_driver()
+    web = get_driver(webenv)
     web.get(oneurl)
-    booktitle = web.title
+    html = web.page_source
+    soup = BeautifulSoup(html,'html.parser')
+    
+    booktitle = filename_clearify(clearify(soup.title))
+
+    local_novel = get_novel_template()
+    local_novel.head.title.string = booktitle
+
+    while(True):
+        bookcontent = clearify(soup)
+
+        chapter = local_novel.new_tag('div',loading='lazy')
+        chaptertext = BeautifulSoup(bookcontent.replace('\n','<br/>\n'),'html.parser')
+        print(chaptertext.text[:100])
+        chapter.insert(0,chaptertext)
+        local_novel.body.append(chapter)
+        next = getnext(soup, oneurl)
+        hasNext = next['hasNext']
+        if not hasNext:
+            break
+        else:
+            nextpage = next['url']
+            web.get(nextpage)
+            html = web.page_source
+            soup = BeautifulSoup(html,'html.parser')
+    print('done')
+
     with open(booktitle+'.html','w',encoding='utf-16') as f:
-        f.write('<!DOCTYPE html><html><head><title>' + booktitle + '</title></head>')
-        f.write('<body>')
-        while(True):
-            bookcontent = web.find_element_by_id('BookContent').text
-            bookcontent = wwwstocx_clearify_callback(bookcontent)
-            f.write('<div loading="lazy">')
-            f.write(bookcontent.replace('。','。<br/>\n'))
-            f.write('</div>\n')
-            nextpage = web.find_element_by_link_text('下壹頁').get_attribute('href')
-            if nextpage is None:
-                f.write('</body>\n</html>\n')
-                break
-            else:
-                web.get(nextpage)
-        print('done')
+        f.write(str(local_novel))
+
     web.close()
     return booktitle
 
-if __name__=="__main__": 
-    f = open("downloadList.txt", "r")
+if __name__=='__main__': 
+    f = open('downloadList.txt', 'r')
     dir_path = dirname(realpath(__file__))
 
     for line in f:
