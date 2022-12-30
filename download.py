7d8346cc9934cf7c37c6b414543b5fb922d83d99
Li-Chi Chang <lichiricky@gmail.com>
2022-12-29 22:03:44 -0500

    add some functions on collections

--- download.py
+++ download.py
@@ -30,12 +30,13 @@ else:
 def get_html_template():
     return BeautifulSoup('<html><head><title></title></head><body></body></html>','html.parser')
 
-def downloadABook_HTML(starturl,endurl):
+def downloadABook_HTML(starturl,endurl,local_novel=None):
     thisbook = onlineBook(starturl,endurl)
     booktitle = thisbook.title
 
-    local_novel = get_html_template()
-    local_novel.head.title.string = booktitle
+    if(local_novel == None):
+        local_novel = get_html_template()
+        local_novel.head.title.string = booktitle
 
     while(True):
         sleep(1 + randrange(200)/100)
@@ -53,34 +54,47 @@ def downloadABook_HTML(starturl,endurl):
         
         local_novel.body.append(htmlchapter)
         local_novel.body.append(htmlcontent)
-
-        with open(join(downloadDoneFolder,booktitle+'.html'),'w',encoding='utf-16') as f:
-                f.write(str(local_novel))
+        
         if(thisbook.goToNextPage()):
-            
             continue
         else:
             break
 
     thisbook.close()
-    return booktitle
+    return (booktitle,local_novel)
 
-def downloadAndPushToAmazon(starturl,endurl):
-    bookname = downloadABook_HTML(starturl,endurl)
+def downloadBooksIntoOne_HTML(starturls,endurls,BooksName='A'):
+    BooksName = BooksName + '_collections'
+    local_novel = get_html_template()
+    local_novel.head.title.string = BooksName
+    for i in range(0,len(starturls)):
+        booktitle,local_novel = downloadABook_HTML(starturls[i],endurls[i],local_novel)
+    return (BooksName,local_novel)
+
+def pushToAmazon(bookname,local_novel):
+    with open(join(downloadDoneFolder,bookname+'.html'),'a',encoding='utf-16') as f: f.write(str(local_novel))
     send_email = send()
     dir_path = join(dirname(realpath(__file__)),downloadDoneFolder)
     send_email.send(EmailFrom, [EmailTo], bookname, '',join(dir_path,bookname+'.html'))
 
+def downloadAndPushToAmazon(starturl,endurl):
+    BooksName,local_novel = downloadABook_HTML(starturl,endurl)
+    pushToAmazon(BooksName,local_novel)
 
 if __name__=="__main__": 
     f = open(downloadListFile, "r")
-    
+    starturls = []
+    endurls = []
 
     for line in f:
         if('#' in line[0]):
             continue
         starturl = line.strip().split(' ')[0]
         endurl = line.strip().split(' ')[1]
+        starturls.append(starturl)
+        endurls.append(endurl)
         # downloadABook_HTML(starturl,endurl)
         downloadAndPushToAmazon(starturl,endurl)
+    # BooksName,local_novel = downloadBooksIntoOne_HTML(starturls,endurls,'bookname')
+    # pushToAmazon(BooksName,local_novel)
     f.close()
